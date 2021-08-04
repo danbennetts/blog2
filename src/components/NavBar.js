9e3e71fff88b37dfe1c0370dd8a5004f5b1ac907
Daniel Bennetts <daniel.bennetts90@gmail.com>
2021-08-04 22:36:49 +1000

    added navbar to blog page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,25 @@
 import React from "react"
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-    return <h1>NavBar Page!</h1>
+    return (
+        <header>
+            <div>
+                <nav>
+                    <NavLink to="/" exact>
+                        the backlog with bentnuts
+                    </NavLink>
+                    <NavLink to="/post">
+                        Blog
+                    </NavLink>
+                    <NavLink to="/project">
+                        Projects
+                    </NavLink>
+                    <NavLink to="/about">
+                        About me
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    )
 }
